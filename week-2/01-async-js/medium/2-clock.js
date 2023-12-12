b8e03e8c89d50e48d6806fd0b2fd6596fa9375bb
DESKTOP-S59BJJM\personal <shahzadahamad285@gmail.com>
2023-12-12 06:53:02 +0530

    Working on medium one

--- week-2/01-async-js/medium/2-clock.js
+++ week-2/01-async-js/medium/2-clock.js
@@ -2,8 +2,8 @@ function clock()
 {
     setInterval(() => {    
        console.clear();
-       var date=new date();
-       console.log(date)       
-}, 1000);
+       var date=new Date();
+       console.log('24 hour format:-'+date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()+'  12 our format:-'+date.getHours()>12?date.getHours()-12:date.getHours()+':'+date.getMinutes()+':'+date.getSeconds()+' '+date.getHours()>12?'PM':'AM'); 
+    }, 1000);
 }
 clock();
