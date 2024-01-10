5d8973132f6bd3a01296b7657d7ce19a77660a80
Shahzad Ahamad <ShahzadAhamad285@gmail.com>
2024-01-10 12:21:40 +0530

    Assignment changes

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useMemo } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
@@ -6,17 +6,29 @@ import { useState } from "react";
 export function Assignment1() {
     const [input, setInput] = useState(0);
     // Your solution starts here
-    const expensiveValue = 0; 
+    let expensiveValue = useMemo(() =>calculateFactorial(input) , [input]);
+
     // Your solution ends here
 
     return (
         <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
+            <input
+                type="number"
+                value={input}
+                onChange={(e) => setInput(Number(e.target.value))}
             />
             <p>Calculated Value: {expensiveValue}</p>
         </div>
     );
+}
+
+function calculateFactorial(num)
+{
+    let res = 1;
+    if (num == 0 || num == 1)
+        return 1;
+    for (let t = num; t >= 1; t--) {
+        res = res * t;
+    }
+    return res;
 }
--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,useMemo } from "react";
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
@@ -16,11 +16,15 @@ for (let i = 0; i < TOTAL_LINES; i++) {
     ALL_WORDS.push(sentence);
 }
 
+
 export function Assignment2() {
     const [sentences, setSentences] = useState(ALL_WORDS);
     const [filter, setFilter] = useState("");
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+    
+    const filteredSentences = useMemo(()=>{
+        setSentences((s)=>setFressSentences());
+        return sentences.filter(x => x.includes(filter))},[sentences,filter]);
 
     return <div>
         <input type="text" onChange={(e) => {
@@ -30,4 +34,19 @@ export function Assignment2() {
             {word}    
         </div>)}
     </div>
+}
+
+function setFressSentences()
+{
+    
+    const new_WORDS = [];
+    for (let i = 0; i < TOTAL_LINES; i++) {
+        let sentence = "";
+        for (let j = 0; j < words.length; j++) {
+            sentence += (words[Math.floor(words.length * Math.random())])
+            sentence += " "
+        }
+        new_WORDS.push(sentence);
+    }
+    return new_WORDS;
 }
