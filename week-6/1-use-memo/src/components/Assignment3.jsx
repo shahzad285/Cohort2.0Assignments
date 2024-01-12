da8d28b64a8e7c1aee4370efba0646e8bc571656
Shahzad Ahamad <ShahzadAhamad285@gmail.com>
2024-01-12 12:06:42 +0530

    Changes for week 6 assignmets

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from 'react';
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
-const Assignment3 = () => {
+export function Assignment3() {
     const [items, setItems] = useState([
         { name: 'Chocolates', value: 10 },
         { name: 'Chips', value: 20 },
@@ -11,11 +11,39 @@ const Assignment3 = () => {
         // Add more items as needed
     ]);
 
+    function AddItem() {
+        const itemName = document.getElementById('item').value;
+        const itemPrice = parseInt(document.getElementById('price').value);
+        if (itemName.length !== 0 && itemPrice > 0) {
+            let item = {
+                name: itemName,
+                value: itemPrice
+            };
+            setItems(items => [...items, item]);
+        }
+    }
+
+
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        let price = 0;
+        items.forEach(element => {
+            price = price + element.value;
+        });
+        return price;
+
+    }, [items]);
+
+
+
+
+
     // Your code ends here
     return (
         <div>
+            <input id="item" type="text" placeholder='Item' /><br />
+            <input id="price" type="number" placeholder='0' /><br />
+            <button onClick={AddItem}>Add Item</button>
             <ul>
                 {items.map((item, index) => (
                     <li key={index}>{item.name} - Price: ${item.value}</li>
@@ -23,7 +51,5 @@ const Assignment3 = () => {
             </ul>
             <p>Total Value: {totalValue}</p>
         </div>
-    );
+    )
 };
-
-export default Assignment3;
