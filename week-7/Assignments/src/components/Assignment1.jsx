8cd4e3695426f6c8dd3ea1143c3e3f67a311cfbf
Shahzad Ahamad <ShahzadAhamad285@gmail.com>
2024-01-19 12:15:15 +0530

    Changes done

--- week-7/Assignments/src/components/Assignment1.jsx
+++ week-7/Assignments/src/components/Assignment1.jsx
@@ -8,8 +8,28 @@ export function Assignment1() {
                     <img src={profilepic} alt="" />
                 </div>
             </div>
-            <div id="body"> <strong class='body-items'>Rita Correia</strong> <span class='body-items'>32</span> <span class='body-items'>London</span></div>
-            <div id="footer"></div>
+            <div id="body">
+                <div="details">
+                    <strong class='body-items'>Rita Correia</strong>
+                    <span class='body-items'>32</span>
+                </div>
+
+                <span class='body-items'>London</span>
+            </div>
+            <div id="footer">
+                <div class="footer-content">
+                    <strong>80K</strong>
+                    <span>Followers</span>
+                </div>
+                <div class="footer-content">
+                    <strong>803K</strong>
+                    <span>Likes</span>
+                </div>
+                <div class="footer-content">
+                    <strong>1.4K</strong>
+                    <span>Photos</span>
+                </div>
+            </div>
         </div>
     </div>
 }
